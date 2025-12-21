08f625da7624a4a80ddc0a6480089d182d295c98
Ondřej Metelka <514098@mail.muni.cz>
2025-12-21 22:50:10 +0100

    devel: work on structs

--- utfpatgen.h
+++ utfpatgen.h
@@ -1,11 +1,27 @@
+#include <stdint.h>
+
+#define MAX_LEVELS 256
+#define TRIE_MAX 100000L
+
 struct trie
 {
+    uint32_t size;
+    char data[TRIE_MAX];
     /* TBD */
 };
 
 struct params
 {
-    /* TBD */
+    uint8_t left_hyphen_min;
+    uint8_t right_hyphen_min;
+    uint8_t n_levels;
+    uint8_t hyph_start;
+    uint8_t hyph_finish;
+    uint8_t pat_start[MAX_LEVELS];
+    uint8_t pat_finish[MAX_LEVELS];
+    uint8_t good_wt[MAX_LEVELS];
+    uint8_t bad_wt[MAX_LEVELS];
+    uint8_t thresh[MAX_LEVELS];
 };
 
 
