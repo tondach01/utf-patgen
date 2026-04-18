e035682401bf71f2a6787ee412ce3bb57cafc25b
Ondřej Metelka <514098@mail.muni.cz>
2026-04-18 14:34:34 +0200

    fix: wrapper

--- utfpatgen_wrapper.sh
+++ utfpatgen_wrapper.sh
@@ -1,9 +1,21 @@
-local dictionary="$1"
-local patterns="$2"
-local output="$3"
-local translate="$4"
+dictionary="$1"
+utfp_dictionary=$dictionary
+patterns="$2"
+output="$3"
+pre_output=$output
+translate="$4"
 
-sed -b 's/1/\xFE\x01/g; s/2/\xFE\x02/g; s/3/\xFE\x03/g; s/4/\xFE\x04/g; s/5/\xFE\x05/g; s/6/\xFE\x06/g; s/7/\xFE\x07/g; s/8/\xFE\x08/g; s/9/\xFE\x09/g' $dictionary > ${dictionary}_utfp
-./build/utfpatgen ${dictionary}_utfp $patterns ${output}_pre $translate
-sed -b 's/\xFE\x01/1/g; s/\xFE\x02/2/g; s/\xFE\x03/3/g; s/\xFE\x04/4/g; s/\xFE\x05/5/g; s/\xFE\x06/6/g; s/\xFE\x07/7/g; s/\xFE\x08/8/g; s/\xFE\x09/9/g' ${output}_pre > $output
-rm -f ${dictionary}_utfp ${output}_pre
+if [ -n "$dictionary" ] && [ -f "$dictionary" ]; then
+    utfp_dictionary=${dictionary}_utfp
+    sed -b 's/1/\xFE\x01/g; s/2/\xFE\x02/g; s/3/\xFE\x03/g; s/4/\xFE\x04/g; s/5/\xFE\x05/g; s/6/\xFE\x06/g; s/7/\xFE\x07/g; s/8/\xFE\x08/g; s/9/\xFE\x09/g' $dictionary > $utfp_dictionary
+fi
+if [ -n "$output" ]; then
+    pre_output=${output}_pre
+fi
+"$( dirname "${BASH_SOURCE[0]}" )/build/utfpatgen" $utfp_dictionary $patterns $pre_output $translate
+if [ -n "$pre_output" ] && [ -f "$pre_output" ]; then
+    sed -b 's/\xFE\x01/1/g; s/\xFE\x02/2/g; s/\xFE\x03/3/g; s/\xFE\x04/4/g; s/\xFE\x05/5/g; s/\xFE\x06/6/g; s/\xFE\x07/7/g; s/\xFE\x08/8/g; s/\xFE\x09/9/g' $pre_output > $output
+fi
+if [ -n "$utfp_dictionary" ] || [ -n "$pre_output" ]; then
+    rm -f $utfp_dictionary $pre_output
+fi
